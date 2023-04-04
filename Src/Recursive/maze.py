e44a7a58d6fa7c4d06c85323e46044341d41b2bb
YoannLeSaint <yoannlesaint@yahoo.fr>
2023-04-04 19:45:44 +0200

    modification maze

--- Src/Recursive/maze.py
+++ Src/Recursive/maze.py
@@ -5,7 +5,7 @@
 ## maze class
 ##
 import numpy as np
-from numpy import array, shape
+from numpy import array, shape, zeros
 import os
 import shutil
 
@@ -75,61 +75,61 @@ import shutil
 class Maze:
     def __init__(self, _maze_folder, maze_name):
         self._maze_folder = _maze_folder
-        self._maze_path = self._maze_folder + '\\' + maze_name + '.txt'
+        self._maze_path = self._maze_folder + '\\' + maze_name
 
         with open(self._maze_path, 'r') as f:
             mat = f.readlines()
-            mat = [ [element.split('\n')[0] for element in mat] ]
-        self._maze = array(mat)
+            line = [element.split('\n')[0] for element in mat]
+        self._maze = array(line)
 
         self._start = (0, 0)
-        self._end = (shape(self._maze)[0]-1, shape(self._maze)[1]-1)
-        print(shape(self._maze))
-        self.visited = np.zeros(shape(self._maze)).astype(bool)
-        self.path = []
+        self._end = (shape(self._maze)[0]-1, len(line[0])-1)
+        self.visited = zeros( self._end ).astype(bool)
+        print(self.visited)
+        self.path = [self._start]
 
     def get_neighbors(self, x, y):
         neighbors = []
-        if x > 0 and not self.visited[x-1, y] and self._maze[x-1, y] != "#":
+        if x > 0 and not self.visited[x-1][y] and self._maze[x-1][y] != "#":
             neighbors.append((x-1, y))
-        if x < self._maze.shape[0]-1 and not self.visited[x+1, y] and self._maze[x+1, y] != "#":
+        if x < self._end[0]-1 and not self.visited[x+1][y] and self._maze[x+1][y] != "#":
             neighbors.append((x+1, y))
-        if y > 0 and not self.visited[x, y-1] and self._maze[x, y-1] != "#":
+        if y > 0 and not self.visited[x][y-1] and self._maze[x][y-1] != "#":
             neighbors.append((x, y-1))
-        if y < self._maze.shape[1]-1 and not self.visited[x, y+1] and self._maze[x, y+1] != "#":
+        if y < self._end[1]-1 and not self.visited[x][y+1] and self._maze[x][y+1] != "#":
             neighbors.append((x, y+1))
         return neighbors
 
     def solve_maze(self): #start_x, start_y, end_x, end_y
         start_x, start_y = self._start
         end_x, end_y = self._end
-        print(self._start, self._end)
-        print(start_x, start_y, end_x, end_y )
         stack = [(start_x, start_y)]
         while stack:
             x, y = stack.pop()
-            self.visited[x, y] = True
+            self.visited[x][y] = True
             if x == end_x and y == end_y:
                 self.path.append((x, y))
                 return True
-            neighbors = self.get_neighbors(x, y)
-            for neighbor_x, neighbor_y in neighbors:
-                stack.append((neighbor_x, neighbor_y))
-                self.path.append((neighbor_x, neighbor_y))
+            else:
+                neighbors = self.get_neighbors(x, y)
+                for neighbor_x, neighbor_y in neighbors:
+                    stack.append((neighbor_x, neighbor_y))
+                    self.path.append((neighbor_y, neighbor_x))
         return False
 
     def __str__(self):
         output = ""
-        for y in range(self._maze.shape[0]):
-            for x in range(self._maze.shape[1]):
+        print(self.path)
+        for x in range(self._end[0]+1):
+            for y in range(self._end[1]+1):
                 if (x, y) == self.path[0]:
                     output += "S"
                 elif (x, y) == self.path[-1]:
                     output += "E"
                 elif self._maze[x][y] == "#":
                     output += "#"
-                elif (x, y) in self.path:
-                    output += "."
+                elif (y, x) in self.path:
+                    output += "·"
                 else:
                     output += " "
             output += "\n"
--- __start__.py
+++ __start__.py
@@ -1,7 +1,11 @@
 import signal
 
 from Src.Menu.menu import Menu
-
+from Src.Recursive.maze import Maze
+import os
+path = os.getcwd()
+path_maze =  "\\".join(path.split("\\") + ["Unsolved"])
+print(path_maze)
 
 def signal_handler(sig, frame):
     print('\nYou pressed Ctrl+C, Bye !')
@@ -9,9 +13,15 @@ def signal_handler(sig, frame):
 
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
-    game = Menu()
-    game.launch_game()
+    # signal.signal(signal.SIGINT, signal_handler)
+    # game = Menu()
+    # game.launch_game()
+    m1 = Maze(path_maze, 'maze_1.txt')
+    m1.print_maze()
+    m1.solve_maze()
+    print(m1)
+
+
 
 
 if __name__ == "__main__":
